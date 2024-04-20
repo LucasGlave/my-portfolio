0b5c07afdb6797225b38f9ed4cde1aa664f2ac9a
LucasGlave <lucasglave7@gmail.com>
2024-04-20 15:55:53 -0300

    fix text movement

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -14,7 +14,6 @@ const Contact = () => {
             className={`w-1/3 absolute bottom-0`}
             style={{ backgroundColor: "rgb(255 129 0)", height: 1 }}
             animate={{
-              // top: ["100%", "0%", "0%", "100%", "100%"],
               left: ["-100%", "100%"],
             }}
             transition={{
@@ -26,11 +25,11 @@ const Contact = () => {
         </div>
       </Reveal>
       <div className="w-5/6 h-5/6 p-24 flex items-center justify-center">
-        <form className=" relative overflow-hidden flex items-center flex-col justify-around w-full h-full shadow-adentro shadow-naranja rounded-lg bg-[rgb(17 14 14)] p-3 px-6">
+        <form className="relative overflow-hidden flex items-center flex-col border border-naranja justify-around w-full h-full shadow-adentro shadow-naranja rounded-lg bg-[rgb(17 14 14)] p-3 px-6">
           <div className="flex flex-col items-center gap-8 justify-evenly h-full w-1/2">
             <div className="w-full h-1/2 flex flex-col gap-3 justify-start">
               <div>
-                <p>Email</p>
+                {/* <p>Email</p> */}
                 <input
                   placeholder="hola@gmail.com"
                   className="relative bg-transparent ring-0 outline-none border border-naranja z-30 text-white placeholder-white text-sm rounded-lg  placeholder-opacity-60 block w-full p-2.5 checked:bg-naranja"
@@ -38,9 +37,9 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <p>Mensaje</p>
+                {/* <p>Mensaje</p> */}
                 <textarea
-                  placeholder="Hola, soy de Buenos Aires, te quiero consultar por..."
+                  placeholder="Buen día, soy de Buenos Aires, te quiero consultar por..."
                   className="relative bg-transparent ring-0 min-h-24 max-h-48 outline-none border border-naranja z-30 text-white placeholder-white text-sm rounded-lg placeholder-opacity-60 block w-full p-2.5 checked:bg-naranja"
                 />
               </div>
@@ -53,10 +52,12 @@ const Contact = () => {
           </div>
         </form>
       </div>
-      <motion.div className="w-full absolute top-[65%]">
+      <motion.div className="w-full absolute bottom-0">
+        {" "}
+        {/* top-[65%] */}
         <motion.h4
-          className="text-[#100f0f] font-bold bottom-0 right-[100%] text-[20rem] w-max h-12 absolute"
-          animate={{ x: "180%" }}
+          className="text-[#131313] bottom-0 font-bold right-[100%] text-[12rem] w-max absolute"
+          animate={{ x: "240%" }}
           transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
         >
           LUCAS GLAVE
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
   return (
     <div className="top-0 fixed left-0 w-full flex flex-row bg-transparent justify-between items-center text-white z-50">
       <div className="bg-fondo w-24 flex items-center py-3 justify-center">
         <h2 className="font-bold">GLAVE</h2>
       </div>
-      <div className="bg-fondo w-24 flex items-center py-3 justify-center">
-        <MenuIcon />
+      <div className="relative w-1/4 flex items-center justify-end">
+        <div className="bg-fondo w-24 py-3 z-10 flex justify-center items-center">
+          <button onClick={toggleNavbar}>
+            <MenuIcon />
+          </button>
+        </div>
+        <AnimatePresence>
+          {navbarOpen && (
+            <motion.div
+              className="absolute w-full flex flex-row justify-start gap-5 items-center rounded-bl-md bg-[#1010106b] backdrop-blur-lg h-full px-3 z-0"
+              initial={{ x: 350 }}
+              animate={{ x: 0 }}
+              exit={{ x: 350 }}
+              transition={{ duration: 1, easings: "linear" }}
+            >
+              <button>INICIO</button>
+              <button>EXPERIENCIA</button>
+              <button>CONTACTO</button>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
